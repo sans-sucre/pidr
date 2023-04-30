13686cab68bcc6975b023d46171aefac21799012
TANG Wenjia <wenjia.tang@telecomnancy.eu>
2023-04-30 22:54:44 +0200

    style : reformater le fichier __main__

--- src/pidr_calcul_diff_angle/__main__.py
+++ src/pidr_calcul_diff_angle/__main__.py
@@ -1,6 +1,6 @@
 """Interface utilisateur"""
 import click
-import statistique 
+import statistique
 import azi_scrapper as scrap
 import ecrire_fichier as ecrire
 import afficher_courbes as afficher
@@ -11,7 +11,6 @@ import afficher_courbes as afficher
 @click.option("-a", default=1, help="Année de donnée à récupérer via SunEarthTools, 1970-2050")
 @click.option("-m", default=1, help="Mois de donnée à récupérer via SunEarthTools, 1-12")
 @click.option("-j", default=1, help="Jour de donnée à récupérer via SunEarthTools, 1-31")
-
 def scrapper(i, a, m, j) -> None:
     data_web = scrap.execute(i, a, m, j)
     # transformer les web data sous forme de liste de string
@@ -24,18 +23,12 @@ def scrapper(i, a, m, j) -> None:
 
     afficher.afficher_parcours("Data/data_web.csv", "Data/data_mes.csv")
 
-    #vérifie si la modélisation est correcte enn affichant l'erreur moyenne sur les valeurs d'azimut et de hauteur 
+    # vérifie si la modélisation est correcte enn affichant l'erreur moyenne sur les valeurs d'azimut et de hauteur
 
-    statistique.modelisation_correcte("Data/data_web.csv","Data/data_ref.csv",0.5)
-    #calcul le decalage d'azimut entre le fichier de referencce et celui de mesure
-    statistique.donne_moyenne_decalage_azimut_corrige("Data/data_ref.csv","Data/data_mes.csv",0.8)
-
+    statistique.modelisation_correcte("Data/data_web.csv", "Data/data_ref.csv", 0.5)
+    # calcul le décalage d'azimut entre le fichier de reference et celui de mesure
+    statistique.donne_moyenne_decalage_azimut_corrige("Data/data_ref.csv", "Data/data_mes.csv", 0.8)
 
 
 if __name__ == '__main__':
     scrapper()
-
-
-
-
-

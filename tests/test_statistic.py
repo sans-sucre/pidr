75de4882724adc5a0d36c8e5ddb19e71a8ebcc3e
TANG Wenjia <wenjia.tang@telecomnancy.eu>
2023-04-30 23:17:22 +0200

    fix: remplacer assertEquals par assertEqual

--- tests/test_statistic.py
+++ tests/test_statistic.py
@@ -7,14 +7,14 @@ class TestCalculStatistique(unittest.TestCase):
 
     def test_erreur_moyenne_absolue(self):
         _v_0 = statistique.donne_erreur_moyenne_absolue([5, 3, 4, 12], [8, 4, 4, 10])
-        self.assertEquals(_v_0, 0.275)
+        self.assertEqual(_v_0, 0.275)
 
         _v_2 = statistique.donne_erreur_moyenne_absolue([4, 4, 4, 4], [4, 4, 4, 4])
-        self.assertEquals(_v_2, 0)
+        self.assertEqual(_v_2, 0)
 
     def test_decalage(self):
         _v_0 = statistique.donne_decalage(10, 30)
-        self.assertEquals(_v_0, 20)
+        self.assertEqual(_v_0, 20)
 
         _v_1 = statistique.donne_decalage(0, 0)
-        self.assertEquals(_v_1, 0)
+        self.assertEqual(_v_1, 0)
